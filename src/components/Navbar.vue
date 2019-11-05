5c8d9747de3c08dad70d5e92376f3543da8764e6
Hoang Hung <20151845@student.hust.edu.vn>
2019-11-06 00:36:39 +0700

    Them navbar, footer, navigate

---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <router-link to="/" class="side_bar_link">
+          <v-list-item link>
+            <v-list-item-action>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </router-link>
+        <router-link to="/about" class="side_bar_link">
+          <v-list-item link>
+            <v-list-item-action>
+              <v-icon>mdi-contact-mail</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Contact</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </router-link>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="indigo" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data: () => ({
+    drawer: null
+  })
+};
+</script>
+
+<style>
+</style>
