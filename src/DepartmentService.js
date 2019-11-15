11e6a691b54e2f9d7b2114b3224683f9081aebdb
Bui Nhat Truong <buinhattruong1996@gmail.com>
2019-11-15 08:38:42 +0700

    Add permission, position role, add UI 15-11-2019

--- src/DepartmentService.js
+++ src/DepartmentService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiBaseURL = "http://18.217.21.235:8083/api/v1/";
-
+const apiGroup5 = "https://dsd05-dot-my-test-project-252009.appspot.com/";
 
 export default class DepartmentService {
   getAllDepartment() {
@@ -53,5 +53,20 @@ export default class DepartmentService {
   deleteDepartment(id)
   {
     return axios.post(`${apiBaseURL}/organizationalStructure/delete?id=${id}`)
+  }
+
+  criteriasDepartment(id)
+  {
+    return axios.get(`http://206.189.34.124:5000/api/group8/kpis?department_id=${id}`)
+  }
+
+  positionDepartment(id)
+  {
+    return axios.get(`${apiBaseURL}/position/findByOrganization?organizationId=${id}`)
+  }
+
+  positionRole(positionId)
+  {
+    return axios.get(`${apiGroup5}/permission/getGroupPermissions?groupId=${positionId}`)
   }
 }
--- src/views/Department.vue
+++ src/views/Department.vue
@@ -1,10 +1,51 @@
 <template>
     <v-container>
-    <div id="handler_bar">
-        <v-btn color="primary">
-            <v-icon>mdi-plus</v-icon>
-            <span>Thêm mới bộ phận</span>
-        </v-btn>
+    <div id="handler_bar">         
+                  <v-dialog v-model="dialogCreate" persistent max-width="600px">
+                    <template v-slot:activator="{ on }">
+                      <v-btn color="primary" dark v-on="on" rounded>
+                        <v-icon>mdi-plus</v-icon>
+                        <span>Thêm mới bộ phận</span></v-btn>
+                    </template>
+                    <v-card>
+                      <v-card-title>
+                        <span class="headline">Thêm mới bộ phận</span>
+                      </v-card-title>
+                      <v-card-text>
+                        <v-container>
+                          <v-row>
+                            <v-col cols="12" sm="6" md="4">
+                              <v-text-field id="departmentCode" label="Mã bộ phận*" required></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                              <v-text-field id="departmentLevelId"
+                                label="Mã cấp bậc*" 
+                                hint="Mã cấp bậc của bộ phận lấy trong bảng cấp bậc bộ phận"
+                               required></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                              <v-text-field id="departmentName"
+                                label="Tên bộ phận*"
+                                hint="Tên chính thức sử dụng cho bộ phận"
+                                persistent-hint
+                                required
+                              ></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                              <v-textarea label="Mô tả" id="txtAreaDescription"></v-textarea>
+                            </v-col>
+                            
+                          </v-row>
+                        </v-container>
+                        <small style="color:red;">* là các trường bắt buộc</small>
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" text @click="dialogCreate = false">Hủy</v-btn>
+                        <v-btn color="blue darken-1" text @click="dialogCreate = false">Thêm mới</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
         <div style="float: right;">
             <v-text-field id="search_input" single-line outlined append-icon="mdi-magnify" placeholder="Nhập để tìm kiếm" />
             <v-select label="Lựa chọn điều kiện lọc" v-bind:items="filter" style="float: left">
@@ -19,14 +60,14 @@
     
     <br>
     <div style="margin-top:10%;">
-<v-simple-table style="width:100%;">
+    <v-simple-table style="width:100%;">
         <template v-slot:default>
             <thead>
                 <tr>
                     <th class="text-center">Mã bộ phận</th>
                     <th class="text-center">Tên bộ phận</th>
                     <th class="text-center">Mô tả</th>
-                    <th class="text-center">Tên cấp bộ phận</th>
+                    <th class="text-center">Cấp bộ phận</th>
                     <th class="text-center">Hành động</th>
                 </tr>
             </thead>
@@ -39,8 +80,8 @@
                     <td>
                         <div class="d-flex justify-space-around">
                 <v-row justify="center">
-                  <v-btn color="primary" dark @click.stop="dialogdetail = true" rounded>Chi tiết</v-btn>
-                  <v-dialog v-model="dialogdetail" max-width="768">
+                  <v-btn color="primary" dark v-on:click="detail(department.id);" rounded>Chi tiết</v-btn>
+                  <!-- <v-dialog v-model="dialogdetail" max-width="768">
                     <v-card>
                       <v-card-title class="headline">Thông tin chi tiết người dùng</v-card-title>
                       <v-card-text>Thông tin người dùng</v-card-text>
@@ -49,7 +90,7 @@
                         <v-btn color="green darken-1" text @click="dialogdetail = false">Đóng</v-btn>
                       </v-card-actions>
                     </v-card>
-                  </v-dialog>
+                  </v-dialog> -->
                 </v-row>
                 <v-row justify="center">
                   <v-dialog v-model="dialogupdate" persistent max-width="600px">
@@ -58,51 +99,35 @@
                     </template>
                     <v-card>
                       <v-card-title>
-                        <span class="headline">Hồ sơ người dùng</span>
+                        <span class="headline">Cập nhật bộ phận</span>
                       </v-card-title>
                       <v-card-text>
                         <v-container>
                           <v-row>
                             <v-col cols="12" sm="6" md="4">
-                              <v-text-field label="Legal first name*" required></v-text-field>
+                              <v-text-field id="departmentCode" label="Mã bộ phận*" required v-model="department.organizationCode"></v-text-field>
                             </v-col>
                             <v-col cols="12" sm="6" md="4">
-                              <v-text-field
-                                label="Legal middle name"
-                                hint="example of helper text only on focus"
-                              ></v-text-field>
+                              <v-text-field id="departmentLevelId"
+                                label="Mã cấp bậc*" 
+                                hint="Mã cấp bậc của bộ phận lấy trong bảng cấp bậc bộ phận"
+                               required v-model="department.organizationLevelId"></v-text-field>
                             </v-col>
                             <v-col cols="12" sm="6" md="4">
-                              <v-text-field
-                                label="Legal last name*"
-                                hint="example of persistent helper text"
+                              <v-text-field id="departmentName"
+                                label="Tên bộ phận*"
+                                hint="Tên chính thức sử dụng cho bộ phận"
                                 persistent-hint
-                                required
+                                required v-model="department.organizationName"
                               ></v-text-field>
                             </v-col>
                             <v-col cols="12">
-                              <v-text-field label="Email*" required></v-text-field>
+                              <v-textarea label="Mô tả" id="txtAreaDescription" v-model="department.organizationDescription"></v-textarea>
                             </v-col>
-                            <v-col cols="12">
-                              <v-text-field label="Password*" type="password" required></v-text-field>
-                            </v-col>
-                            <v-col cols="12" sm="6">
-                              <v-select
-                                :items="['0-17', '18-29', '30-54', '54+']"
-                                label="Age*"
-                                required
-                              ></v-select>
-                            </v-col>
-                            <v-col cols="12" sm="6">
-                              <v-autocomplete
-                                :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                                label="Interests"
-                                multiple
-                              ></v-autocomplete>
-                            </v-col>
+                            
                           </v-row>
                         </v-container>
-                        <small>*indicates required field</small>
+                        <small style="color:red;">* là các trường bắt buộc</small>
                       </v-card-text>
                       <v-card-actions>
                         <v-spacer></v-spacer>
@@ -140,7 +165,7 @@
     <div class="tab">
         <v-btn class="tablinks" v-on:click="openCity(event, 'departmentLevel')">Cấp bậc bộ phận</v-btn>
         <v-btn class="tablinks" v-on:click="openCity(event, 'departmentUser')">Cơ cấu bộ phận</v-btn>
-        <v-btn class="tablinks" v-on:click="openCity(event, 'position')">Chức danh</v-btn>
+        <v-btn class="tablinks" v-on:click="openCity(event, 'position')">Phân quyền theo chức danh</v-btn>
         <v-btn class="tablinks" v-on:click="openCity(event, 'kpiEquation')">Lưới tiêu chí KPI</v-btn>
         <v-btn class="tablinks" v-on:click="openCity(event, 'log')">Nhật ký hoạt động</v-btn>
     </div> 
@@ -150,7 +175,7 @@
             <template v-slot:default>
                 <thead>
                     <tr>
-                        <th class="text-center">STT</th>
+                        <th class="text-center">Mã cấp bậc</th>
                         <th class="text-center">Tên cấp bộ phận</th>
                         <th class="text-center">Mô tả</th>
                     </tr>
@@ -165,12 +190,176 @@
             </template>
             </v-simple-table>
         </div>
-        <div id="departmentUser" class="tabcontent">2</div>
-        <div id="position" class="tabcontent">3</div>
-        <div id="kpiEquation" class="tabcontent">4</div>
-        <div id="log" class="tabcontent">5</div>
+        <div id="departmentUser" class="tabcontent">
+          <v-row style="margin-left:0.5%; margin-top:0.5%; margin-bottom:0.5%;">
+            <v-dialog v-model="dialogPosition" persistent max-width="200px">
+                    <template v-slot:activator="{ on }">
+                      <v-btn color="primary" dark v-on="on" rounded>
+                        <v-icon>mdi-plus</v-icon>
+                        <span>Thêm mới</span></v-btn>
+                    </template>
+                    <v-card>
+                      <v-card-title>
+                        <span class="headline">Thêm mới chức danh</span>
+                      </v-card-title>
+                      <v-card-text>
+                        <v-container>
+                          <v-row>
+                            <v-col cols="12" sm="6" md="4">
+                              <v-text-field id="departmentCode" label="Mã bộ phận*" required></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                              <v-text-field id="departmentLevelId"
+                                label="Mã cấp bậc*" 
+                                hint="Mã cấp bậc của bộ phận lấy trong bảng cấp bậc bộ phận"
+                               required></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                              <v-text-field id="departmentName"
+                                label="Tên bộ phận*"
+                                hint="Tên chính thức sử dụng cho bộ phận"
+                                persistent-hint
+                                required
+                              ></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                              <v-textarea label="Mô tả" id="txtAreaDescription"></v-textarea>
+                            </v-col>
+                            
+                          </v-row>
+                        </v-container>
+                        <small style="color:red;">* là các trường bắt buộc</small>
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" text @click="dialogPosition = false">Hủy</v-btn>
+                        <v-btn color="blue darken-1" text @click="dialogPosition = false">Thêm mới</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+          </v-row>
+          <v-simple-table style="width:100%;">
+          <template>
+              <thead>
+                    <tr>
+                        <th class="text-center">Mã chức danh</th>
+                        <th class="text-center">Tên chức danh</th>
+                        <th class="text-center">Cấp bộ phận</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(pos) in position" :key="pos.id">
+                        <td class="text-center">{{ pos.id}}</td>
+                        <td class="text-center">{{ pos.positionName }}</td>
+                        <td class="text-center">{{ pos.levelId}}</td>
+                    </tr>
+                </tbody>
+            </template>
+          </v-simple-table>
+        </div>
+        <div id="position" class="tabcontent">
+           <v-row style="margin-left:0.5%; margin-top:0.5%; margin-bottom:0.5%;">
+            <v-dialog v-model="dialogPositionRole" persistent max-width="200px">
+                    <template v-slot:activator="{ on }">
+                      <v-btn color="primary" dark v-on="on" rounded>
+                        <v-icon>mdi-plus</v-icon>
+                        <span>Thêm mới</span></v-btn>
+                    </template>
+                    <v-card>
+                      <v-card-title>
+                        <span class="headline">Thêm mới chức danh</span>
+                      </v-card-title>
+                      <v-card-text>
+                        <v-container>
+                          <v-row>
+                            <v-col cols="12" sm="6" md="4">
+                              <v-text-field id="departmentCode" label="Mã bộ phận*" required></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                              <v-text-field id="departmentLevelId"
+                                label="Mã cấp bậc*" 
+                                hint="Mã cấp bậc của bộ phận lấy trong bảng cấp bậc bộ phận"
+                               required></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                              <v-text-field id="departmentName"
+                                label="Tên bộ phận*"
+                                hint="Tên chính thức sử dụng cho bộ phận"
+                                persistent-hint
+                                required
+                              ></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                              <v-textarea label="Mô tả" id="txtAreaDescription"></v-textarea>
+                            </v-col>
+                            
+                          </v-row>
+                        </v-container>
+                        <small style="color:red;">* là các trường bắt buộc</small>
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" text @click="dialogPositionRole = false">Hủy</v-btn>
+                        <v-btn color="blue darken-1" text @click="dialogPositionRole = false">Thêm mới</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+          </v-row>
+          <v-simple-table style="width:100%;">
+          <template>
+              <thead>
+                    <tr>
+                        <th class="text-center">Mã chức danh</th>
+                        <th class="text-center">Mã phân quyền</th>
+                        <th class="text-center">Ngày tạo</th>
+                        <th class="text-center">Ngày cập nhật</th>
+                        <th class="text-center">Tên quyền</th>
+                        <th class="text-center">Mô tả</th>
+                        <th class="text-center">url</th>
+                        <th class="text-center">Trạng thái</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(posRole) in positionRoleArr" :key="posRole.id">
+                        <td class="text-center">{{ 1}}</td>
+                        <td class="text-center">{{ posRole.id }}</td>
+                        <td class="text-center">{{ posRole.createdDate}}</td>
+                        <td class="text-center">{{ posRole.lastUpdated}}</td>
+                        <td class="text-center">{{ posRole.name }}</td>
+                        <td class="text-center">{{ posRole.description}}</td>
+                        <td class="text-center">{{ posRole.url }}</td>
+                        <td class="text-center">{{ posRole.statusId}}</td>
+                    </tr>
+                </tbody>
+            </template>
+          </v-simple-table>
+        </div>
+        <div id="kpiEquation" class="tabcontent"><v-simple-table style="width:100%;">
+
+            <template>
+              <thead>
+                    <tr>
+                        <th class="text-center">Mã bộ phận</th>
+                        <th class="text-center">Tên tiêu chí KPI</th>
+                        <th class="text-center">Tỉ lệ tiêu chí</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- <tr v-for="(pos) in criteriasDatas" :key="pos.id">
+                        <td class="text-center">{{ pos.id}}</td>
+                        <td class="text-center">{{ pos.positionName }}</td>
+                        <td class="text-center">{{ pos.levelId}}</td>
+                    </tr> -->
+                </tbody>
+            </template>
+          </v-simple-table></div>
+        <div id="log" class="tabcontent">
+          
+        </div>
     </div>
     </v-container>
+
+
 </template>
 
 
@@ -221,10 +410,16 @@ export default {
     return {
       datas: [],
       levelDatas:[],
+      criteriasDatas:[],
+      position:[],
+      positionRoleArr:[],
       filter: [
         "Tên bộ phận",
         "Cấp bộ phận",
       ],
+      dialogCreate: false,
+      dialogPosition: false,
+      dialogPositionRole:false,
       dialogdetail: false,
       dialogupdate: false,
       dialogdelete: false
@@ -258,7 +453,30 @@ export default {
       var response = await departmentService.getAllDepartment();
       this.datas = response.data;
       location.reload();
+    },
+    async criteriasDepartment(id)
+    {
+      var response = await departmentService.criteriasDepartment(id);
+      this.criteriasDatas = response.data;
+    },
+    async positionDepartment(id)
+    {
+      var positionResponse = await departmentService.positionDepartment(id);
+      this.position = positionResponse.data;
+    },
+    async positionRole(positionId)
+    {
+      var positionRole = await departmentService.positionRole(positionId);
+      this.positionRoleArr = positionRole.data;
+    },
+    async detail(id)
+    {
+      alert("Dữ liệu chi tiết được nạp vào tab con bên dưới");
+      this.criteriasDepartment(id);
+      this.positionDepartment(id);
+      this.positionRole(id);
     }
+
   },
   async created()
   {
